e2f35b0aad2dad6f7992eed29ccb60000cdbea87
KelvinMarpaung <yohaneskelvin9@gmail.com>
2024-09-06 15:20:51 +0700

    telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,1 +1,42 @@
-// NIM - Your Name
+// 12S24043 - DESNITA PARDOSI
+// 12S24018 - KELVIN  YOHANES PUTRA
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN;
+
+        iSBN = input.nextLine();
+        String jUDUL;
+
+        jUDUL = input.nextLine();
+        String pENULIS;
+
+        pENULIS = input.nextLine();
+        int tAHUNTERBIT;
+
+        tAHUNTERBIT = Integer.parseInt(input.nextLine());
+        String pENERBIT;
+
+        pENERBIT = input.nextLine();
+        String fORMATBUKU;
+
+        fORMATBUKU = input.nextLine();
+        double hARGAPEMBELIAN;
+
+        hARGAPEMBELIAN = Double.parseDouble(input.nextLine());
+        double mINIMUMMARGIN;
+
+        mINIMUMMARGIN = Double.parseDouble(input.nextLine());
+        int sTOK;
+
+        sTOK = Integer.parseInt(input.nextLine());
+        double rATING;
+
+        rATING = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + jUDUL + "|" + pENULIS + "|" + tAHUNTERBIT + "|" + pENERBIT + "|" + fORMATBUKU + "|" + hARGAPEMBELIAN + "|" + mINIMUMMARGIN + "|" + sTOK + "|" + rATING);
+    }
+}
